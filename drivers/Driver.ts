5313de866fdb4d2823a27e991f707792a6ce12d3
Alexey Krainev <xmrvizzy@ya.ru>
2024-01-05 02:43:51 +0500

    Новая инициализация устройства

--- drivers/Driver.ts
+++ drivers/Driver.ts
@@ -4,11 +4,11 @@ import SwitcherApp from '../app';
 export default class SwitcherDriver extends Homey.Driver {
     async onPairListDevices() {
         const app = this.homey.app as SwitcherApp;
-        const devices = app.devices;
+        const switchers = app.switchers;
         const models = this.manifest.models;
 
-        return Object.values<any>(devices)
-            .filter(device => models.includes(device.type))
+        return Object.values<any>(switchers)
+            .filter(switcher => models.includes(switcher.type))
             .map(({ name, device_id }) => ({ name, data: { id: device_id } }));
     }
 }
